4726d40e9bfd4f9ee456445517dd2d1225ca326f
lilxsamor <samuel.moor@band.ch>
2025-03-21 13:07:30 +0100

    Added poll upload & poll options

--- src/app/services/user-post.service.ts
+++ src/app/services/user-post.service.ts
@@ -14,6 +14,10 @@ export class UserPostService {
     this.userPostsRef = db.list(this.dbPath);
   }
 
+  vote(id: number, optionIndex: number) {
+    
+  }
+
   getAll(): AngularFireList<Post> {
     return this.userPostsRef;
   }
